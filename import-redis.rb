b5ef561bc1e589bebd74cc10563b1e3afbafebc6
Per Lundberg <per@halleluja.nu>
2013-04-19 22:42:13 +0300

    Added simple script for importing data into a Redis database.

---
+++ import-redis.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env jruby
+
+require 'bson'
+require 'json'
+require 'redis'
+
+products = JSON.parse(IO.read('db/products.json'))
+
+redis = Redis.new
+
+products.each do |product|
+  product.delete 'imageUrl'
+  product.delete 'objectId'
+  product.delete 'slug'
+
+  key = BSON::ObjectId.new.to_s
+
+  redis.hset 'products', key, product
+end
